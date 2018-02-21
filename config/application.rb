98ae5dfb722edb70e24be914d351cfb45963bdb1
Ramon <ramonlrodriguez@gmail.com>
2018-02-20 19:09:23 -0600

    update for heroku images

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,8 @@ module FinalProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.serve_static_assets = true
-    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
     config.assets.compile = true
+    config.assets.digest = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
